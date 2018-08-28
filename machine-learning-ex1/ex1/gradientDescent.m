1562e19f8b9d0d183235ac66731250aa1ab47114
Dylan-Shao <yangshao2014@gmail.com>
2018-08-28 01:55:26 -0700

    ex1:Gradient Descent (for One Variable) solution

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,6 +7,9 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+x1 = X(:,1);
+x2 = X(:,2);
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -17,11 +20,16 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    delta1 = 0;
+    delta2 = 0;
 
+    for i = 1:m
+        delta1 = delta1 + alpha*(theta(1)*x1(i) + theta(2)*x2(i) - y(i))/m;
+        delta2 = delta2 + alpha*((theta(1)*x1(i) + theta(2)*x2(i) - y(i))*x2(i))/m;
+    end
 
-
-
-
+    theta(1) = theta(1) - delta1;
+    theta(2) = theta(2) - delta2;
 
     % ============================================================
 
